fdbe5055ffd394def9a64925bdba0761df1c3ce7
RT503 <musinspiration@yandex.ru>
2021-05-15 02:04:54 +0300

    props children learning

--- src/components/App.js
+++ src/components/App.js
@@ -1,5 +1,4 @@
 import React from 'react';
-import { useState, useEffect } from 'react';
 import Header from "./Header";
 import Main from "./Main.js";
 import Footer from "./Footer";
@@ -28,8 +27,6 @@ function App() {
         setIsAddPlacePopupOpen(!isAddPlacePopupOpen);
     }
 
-
-
     function closeAllPopups() {
         setIsEditProfilePopupOpen(false);
         setIsAddPlacePopupOpen(false);
@@ -53,15 +50,34 @@ function App() {
                 isOpen={isEditProfilePopupOpen}
                 onClose={closeAllPopups}
 
-            />
+            >
+
+                    <input type="text" id="profileName" className="popup__input popup__input_type_name" name="name"
+                           placeholder="Имя" required minLength="2" maxLength="40"/>
+                    <span className="popup__input-error profileName-error"></span>
+                    <input type="text" id="profileStatus" className="popup__input popup__input_type_status" name="status"
+                           placeholder="Обо мне" required minLength="2" maxLength="200"/>
+                    <span className="popup__input-error profileStatus-error"></span>
+
+            </PopupWithForm>
+
             <PopupWithForm
                 name="add-new-card"
-                title="Создать"
-                submitText="Да"
+                title="Новое место"
+                submitText="Сохранить"
                 isOpen={isAddPlacePopupOpen}
                 onClose={closeAllPopups}
 
-            />
+            >
+                <input type="text" id="placeName" className="popup__input popup__input_type_name" name="name"
+                       placeholder="Название" required minLength="2" maxLength="30"/>
+                <span className="popup__input-error placeName-error"></span>
+                <input type="url" id="placeLink" className="popup__input popup__input_type_picture-link" name="link"
+                       placeholder="Ссылка на картинку" required pattern="https://.*"/>
+                <span className="popup__input-error placeLink-error"></span>
+
+            </PopupWithForm>
+
             <PopupWithForm
                 name="edit-avatar"
                 title="Обновить аватар"
@@ -69,7 +85,20 @@ function App() {
                 isOpen={isEditAvatarPopupOpen}
                 onClose={closeAllPopups}
 
-            />
+            >
+
+                <input
+                    id="avatar"
+                    type="url"
+                    className="popup__input popup__input_type_avatar-link"
+                    name="avatar"
+                    placeholder="Ссылка на картинку"
+                    required
+                />
+                <span className="popup__input-error avatar-error"></span>
+
+            </PopupWithForm>
+
             <PopupWithForm
                 name="popup_confirm"
                 title="Вы уверены?"
@@ -77,6 +106,7 @@ function App() {
                 onClose={closeAllPopups}
 
             />
+
             <ImagePopup />
 
         </div>
--- src/components/PopupWithForm.js
+++ src/components/PopupWithForm.js
@@ -7,8 +7,10 @@ function PopupWithForm (props) {
             <div className="popup__content">
                 <button type="button" className="popup__close" onClick={props.onClose}></button>
                 <h3 className="popup__title">{props.title}</h3>
+
                 <form className="popup__form" name={props.name} noValidate>
-                    <button type="submit" className="popup__submit-button">{props.submitText}</button>
+                    {props.children}
+                    <button type="submit" form={props.name} className="popup__submit-button">{props.submitText}</button>
                 </form>
             </div>
         </div>
